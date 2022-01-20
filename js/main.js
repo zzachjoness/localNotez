fdd2f392053dd012c6d02aaa36ee0a05de73db25
zzachjoness <zzachjoness@gmail.com>
2022-01-20 13:29:44 -0500

    addName fx add

--- js/main.js
+++ js/main.js
@@ -2,17 +2,24 @@ const form = document.getElementById("uniqueID");
 let names = document.getElementById("nameList");
 let namesArr = [];
 let myStorage = window.localStorage;
+function addName(name) {
+  let div = document.createElement("div");
+  div.innerText = name;
+  names.appendChild(div);
+}
 for (let i = 0; i < myStorage.length; i++) {
   const getName = myStorage.getItem(myStorage.key(i));
   namesArr.push(getName);
-  console.log(namesArr);
 }
-names.innerHTML = namesArr.map((name) => `<div>${name}</div>`);
+namesArr.forEach((name) => {
+  addName(name);
+});
 form.addEventListener("submit", (e) => {
   const length = myStorage.length;
   e.preventDefault();
   const name = e.target.name.value;
-  console.log("name: ", name);
-  console.log("length: ", length);
   myStorage.setItem(`uid${length}`, name);
+  namesArr.push(name);
+  addName(name);
+  document.getElementById("name").value = "";
 });
