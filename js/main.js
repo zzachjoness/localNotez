be3f75dba0b4d2684d5c5f03896b215f71c553d7
zzachjoness <zzachjoness@gmail.com>
2022-01-20 16:20:03 -0500

    addName fx button add

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ let namesArr = [];
 let myStorage = window.localStorage;
 function addName(name) {
   let div = document.createElement("div");
+  let button = document.createElement("button");
+  button.innerText = "-";
+  button.onclick = function () {
+    alert("here be dragons");
+  };
   div.innerText = name;
+  div.appendChild(button);
   names.appendChild(div);
 }
 for (let i = 0; i < myStorage.length; i++) {
@@ -15,10 +21,10 @@ namesArr.forEach((name) => {
   addName(name);
 });
 form.addEventListener("submit", (e) => {
-  const length = myStorage.length;
+  const key = `uid${myStorage.length}`;
   e.preventDefault();
   const name = e.target.name.value;
-  myStorage.setItem(`uid${length}`, name);
+  myStorage.setItem(key, name);
   namesArr.push(name);
   addName(name);
   document.getElementById("name").value = "";
