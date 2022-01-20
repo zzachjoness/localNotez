64fe54a4d49be620e4139d08b035cfea7bb21bc1
zzachjoness <zzachjoness@gmail.com>
2022-01-19 22:07:59 -0500

    js name map fx

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 const form = document.getElementById("uniqueID");
 let names = document.getElementById("nameList");
+let namesArr = [];
+let myStorage = window.localStorage;
+for (let i = 0; i < myStorage.length; i++) {
+  const getName = myStorage.getItem(myStorage.key(i));
+  namesArr.push(getName);
+  console.log(namesArr);
+}
+names.innerHTML = namesArr.map((name) => `<div>${name}</div>`);
 form.addEventListener("submit", (e) => {
+  const length = myStorage.length;
   e.preventDefault();
-  const input = e.target;
-  console.log(input.name.value);
-  names.innerHTML = input.name.value;
+  const name = e.target.name.value;
+  console.log("name: ", name);
+  console.log("length: ", length);
+  myStorage.setItem(`uid${length}`, name);
 });
